75cdcb872d76cfdb3d7446508f03edd482339cd2
tiptopyu <tiptopflum@me.com>
2016-10-28 10:24:02 +0900

    1028

--- Arduino_receive/Source.cpp
+++ Arduino_receive/Source.cpp
@@ -6,10 +6,14 @@
 #include <iostream>
 using namespace std;
 
-void main()
+
+
+void Euler_receive(int arduinoCOM, HANDLE hComm, float Euler[3])
 {
-	int arduinoCOM=71;
-	HANDLE hComm;
+
+	double rob_x, rob_y, rob_rad;
+
+	
 	//シリアルポートを開いてハンドルを取得
 	string com = "\\\\.\\COM" + to_string(arduinoCOM);
 	hComm = CreateFile(com.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -18,9 +22,7 @@ void main()
 		char z;
 		z = getchar();
 		return;
-	}
-	//ポートを開けていれば通信設定を行う
-	else
+	}else
 	{
 		DCB lpTest;
 		GetCommState(hComm, &lpTest);
@@ -30,28 +32,26 @@ void main()
 		lpTest.StopBits = ONESTOPBIT;
 		SetCommState(hComm, &lpTest);
 	}
-	printf("opened!!\n");
-
-
+	
 	bool isInitialized = false;
 	unsigned char	sendbuf[1];
 	unsigned char	receive_data[30] = {};
 	int				ret;
-	float			DL, DR, DIS, ANG;
 	unsigned long	len;
 	char *euler_1, *euler_2, *euler_3;
-	float Euler[3];
 	int i = 0;
-	float			droidOrientation[3];
-	Sleep(2000);
+	Sleep(500);
 
-	while (i<=20)
+	while (1)
 	{
-		i++;
+		if (GetAsyncKeyState(VK_SPACE)==1)
+		{
+			break;
+		}
 
-		getchar();
+		//getchar();
 		// ハンドルチェック
-		if (!hComm)	return;
+//		if (!hComm)	return;
 		// バッファクリア
 		memset(sendbuf, 0x00, sizeof(sendbuf));
 		// パケット作成
@@ -80,15 +80,59 @@ void main()
 		euler_1 = strtok((char*)receive_data, ",");
 		euler_2 = strtok(NULL, ",");
 		euler_3 = strtok(NULL, ",");
-		//printf("%s\n", receive_data);
+		printf("%s\n", receive_data);
 
 		Euler[0] = strtod(euler_1, NULL);
 		Euler[1] = strtod(euler_2, NULL);
 		Euler[2] = strtod(euler_3, NULL);
+
 		printf("%f\n", Euler[0]);
 		printf("%f\n", Euler[1]);
 		printf("%f\n", Euler[2]);
 	}
+}
+
+void init_arduino(int arduinoCOM, HANDLE hComm, float Euler[3])
+{
+	string com = "\\\\.\\COM" + to_string(arduinoCOM);
+	hComm = CreateFile(com.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hComm == INVALID_HANDLE_VALUE){
+		printf("シリアルポートを開くことができませんでした。");
+		char z;
+		z = getchar();
+		return;
+	}
+	else
+	{
+		DCB lpTest;
+		GetCommState(hComm, &lpTest);
+		lpTest.BaudRate = 115200;
+		lpTest.ByteSize = 8;
+		lpTest.Parity = NOPARITY;
+		lpTest.StopBits = ONESTOPBIT;
+		SetCommState(hComm, &lpTest);
+	}
+	
+
+}
+
+void main()
+{
+
+	
+	
+	int arduinoCOM=71;
+	HANDLE hComm=0;
+	float Euler[3], initEuler[3];
+	
+
+	printf("init_arduino_in");
+	//init_arduino(arduinoCOM, hComm, initEuler);
+	
+	printf("Euler_receive_in");
+	Euler_receive(arduinoCOM, hComm, Euler);
+
+
 }
 
 
